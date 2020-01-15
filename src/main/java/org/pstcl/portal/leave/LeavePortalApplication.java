6d5ddefe243a6ac41ae75d6a19e2128e4f011343
pstcl <40257916+pstcl@users.noreply.github.com>
2020-01-15 14:57:09 +0530

    sdsd

--- src/main/java/org/pstcl/portal/leave/LeavePortalApplication.java
+++ src/main/java/org/pstcl/portal/leave/LeavePortalApplication.java
@@ -6,15 +6,7 @@ import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class LeavePortalApplication {
-
-	//test
 	public static void main(String[] args) {
 		ApplicationContext applicationContext= SpringApplication.run(LeavePortalApplication.class, args);
-//		LeaveTypeRepository leaveTypeRepository= applicationContext.getBean(LeaveTypeRepository.class);
-//		LeaveApplicationRepository applicationRepository= applicationContext.getBean(LeaveApplicationRepository.class);
-//		List<Leave> list=new ArrayList<Leave>();
-//		leaveTypeRepository.findAll().forEach(leaveType->{Leave  leave=new Leave(leaveType);list.add(leave);});
-//		applicationRepository.findAll().forEach(leaveApplication->{leaveApplication.setLeaveDetails(list);applicationRepository.save(leaveApplication);});
 	}
-
 }
